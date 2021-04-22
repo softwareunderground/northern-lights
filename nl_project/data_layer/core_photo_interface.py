3ab7244636eb51ebb42b346d627c8abeda3c1139
nathangeology <nathan_geology@utexas.edu>
2021-04-21 17:23:50 -0700

    Made functions to sample the core photos and clean up and join the core photos into longer intervals. Also, set up a caching mechanism to speed up data exploration after the photos are downloaded.

--- nl_project/data_layer/core_photo_interface.py
+++ nl_project/data_layer/core_photo_interface.py
@@ -1,6 +1,11 @@
+import pandas as pd
+
 from .well_data import WellInterface
 from nl_project.input_layer.get_data import GetProjectData
 from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.filters import sobel
 
 
 class CorePhotoInterface(WellInterface):
@@ -12,12 +17,54 @@ class CorePhotoInterface(WellInterface):
         print('WARNING: Loading core photos, may take a long time the first time!')
         self.data = self.get_data(key='Core Photo Data')
 
+    def report_available_core_photos(self) -> pd.DataFrame:
+        return self.core_photo_metadata
+
+    def get_sample_at_depth(self, md, sample_thickness=0.08) -> object:
+        photos = self.get_photos_for_depth_range(md - sample_thickness, md + sample_thickness)
+        sample_photos = photos['filtered_photos']
+        top_idx = sample_photos.columns.get_loc('top_depth')
+        btm_idx = sample_photos.columns.get_loc('bottom_depth')
+        photo_top = sample_photos.iat[0, top_idx]
+        photo_btm = sample_photos.iat[sample_photos.shape[0] - 1, btm_idx]
+        if sample_photos.empty:
+            print('no core photos for this depth')
+            return None
+        else:
+            photo = photos['interval 1']
+            photo_thickness = photo_btm - photo_top
+            px_per_m = photo.shape[0] / photo_thickness
+            start_pix = int((md - sample_thickness - photo_top) * px_per_m)
+            end_pix = int((md + sample_thickness - photo_top) * px_per_m)
+            photo_sample = photo[start_pix:end_pix, 50:350, :]
+            return photo_sample
+
     def get_photos_for_depth_range(self, start_md, end_md):
-        start_md_filter = float(int(start_md) - 1)
-        top_filter = self.core_photo_metadata['top_depth'] > start_md_filter
-        bottom_filter = self.core_photo_metadata['end_depth'] < end_md
+        start_md_filter = float(int(start_md))
+        top_filter = self.core_photo_metadata['top_depth'] >= start_md_filter
+        bottom_filter = self.core_photo_metadata['bottom_depth'] < end_md + 1.0
         filtered_photos = self.core_photo_metadata.loc[top_filter & bottom_filter, :]
-        self._clean_up_photos(filtered_photos)
+        filtered_photos = filtered_photos.loc[~filtered_photos['top_depth'].duplicated()]
+        change_points = filtered_photos['top_depth'].diff() > 4.0
+        list_of_core_changes = list(change_points.loc[change_points == True].index)
+        list_of_core_changes = [0] + list_of_core_changes
+        photos_out = {}
+        interval = 1
+        for ind, x in enumerate(list_of_core_changes):
+
+            if x != list_of_core_changes[-1]:
+                photos = filtered_photos.loc[x:list_of_core_changes[ind + 1]]
+                meta = self.core_photo_metadata.loc[x:list_of_core_changes[ind + 1]]
+            else:
+                photos = filtered_photos.loc[x:]
+                meta = self.core_photo_metadata.loc[x:]
+            photos_out[f'interval {interval}'] = self._clean_up_photos(photos)
+            photos_out[f'interval {interval} top'] = meta.loc[x, 'top_depth']
+            photos_out[f'interval {interval} bottom'] = meta.loc[x, 'top_depth']
+            interval += 1
+        photos_out['filtered_photos'] = filtered_photos
+        return photos_out
+
 
     def _load_from_sources(self, key):
         # Load the core metadata table
@@ -34,5 +81,26 @@ class CorePhotoInterface(WellInterface):
             self.loaded_photos[idx] = f'core_photo{idx}'
 
     def _clean_up_photos(self, photos_df):
+        output_photos = []
         for idx, row in photos_df.iterrows():
-            print('here')
+            current_photo = self.get_data(key=f'core_photo{idx}', load=False)[0]['image_array']
+            current_photo = current_photo[:, 75:, :]
+
+            max_val = 255 * current_photo.shape[0]
+            max_val_rows = 255 * current_photo.shape[1]
+            brightness = current_photo[:, :, 0] * 0.21 + current_photo[:, :, 1] * 0.72 + current_photo[:, :, 2] * 0.07
+            # plt.hist(np.sum(brightness, axis=0)/max_val)
+            brightness_filter = ((np.sum(brightness, axis=0)/max_val) > 0.3) & ((np.sum(brightness, axis=0)/max_val) < 0.98)
+
+            row_brightness_filter = np.sum(brightness, axis=1) / max_val_rows > 0.1
+            cleaned_photo = current_photo[:, brightness_filter, :]
+            # Check for header at the top
+            if np.sum(~row_brightness_filter[:230]) > 60:
+                cleaned_photo = cleaned_photo[230:, :, :]
+            cleaned_photo = cleaned_photo[:, :900, :]
+            output_photos.append(cleaned_photo)
+            if cleaned_photo.shape[1] < 400:
+                im = Image.fromarray(cleaned_photo)
+                im.show()
+        combined_photo = np.concatenate(output_photos, axis=0)
+        return combined_photo
--- nl_project/test/test_core_photo_interface.py
+++ nl_project/test/test_core_photo_interface.py
@@ -1,7 +1,17 @@
 from nl_project.data_layer.core_photo_interface import CorePhotoInterface
+import numpy as np
 
 
 class TestCorePhotoInterface:
     def test_get_photos_for_depth_range(self):
         interface = CorePhotoInterface('test_well')
-        print('here')
+        photos = interface.get_photos_for_depth_range(2592.5, 2649.0)
+        if len(photos) < 5:
+            raise Exception('Test Failed')
+
+    def test_get_sample_at_depth(self):
+        interface = CorePhotoInterface('test_well')
+        photo_sample = interface.get_sample_at_depth(2593.1)
+        if not isinstance(photo_sample, np.ndarray):
+            raise Exception('Test Failed')
+
